74030291197fbafde9e7e1804c3d0ecc08270884
Riyuo <arcx@Riyuos-MacBook-Air.local>
2026-01-16 17:02:21 +0800

    Subject: Update

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/image/Logo.webp"
+
+
+export default function Navbar() {
+    
+
+    return(
+
+        <div className="navbar-container">
+            <img src={logo} alt="" srcset="" />
+        </div>
+    )
+}
